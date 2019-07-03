1b5318ef44a3dfec4d7411c5ebcb6e3c09d4f1a6
dkumar <dkumar@lbl.gov>
2019-07-03 23:50:21 +0000

    Added script for generatign scattering data

--- src/dft.cpp
+++ src/dft.cpp
@@ -6,6 +6,7 @@ namespace py = pybind11;
 typedef std::complex<float> complex_t;
 
 const complex_t COMPLX_J(0, 1);
+const complex_t COMPLX_ZERO(0, 0);
 
 py::array py_dft(py::array_t<float, py::array::c_style | py::array::forcecast> Pts,
 				 py::array_t<float, py::array::c_style | py::array::forcecast> qVals) {
@@ -16,23 +17,24 @@ py::array py_dft(py::array_t<float, py::array::c_style | py::array::forcecast> P
 		if ((Pts.shape()[1] != 3) or (qVals.shape()[1] != 3))
 			throw std::runtime_error("input arrays must of shape [N, 3]");
 
+		/* size of input arrays */
+		unsigned npts = Pts.shape()[0];
+		unsigned nq = qVals.shape()[0];
+
 		/* NumPy  will allocate the buffer */
-		auto result = py::array_t<complex_t>(qVals.shape()[0]);
+		auto result = py::array_t<complex_t>(nq);
 
 		py::buffer_info buf1 = Pts.request();
 		py::buffer_info buf2 = qVals.request();
 		py::buffer_info buf3 = result.request();
 		
-
 		float * pts = (float *) buf1.ptr;
 		float * qvs = (float *) buf2.ptr;
 		complex_t * ft = (complex_t *) buf3.ptr;
 
-		unsigned npts = Pts.shape()[0];
-		unsigned nq = qVals.shape()[0];
 #pragma omp parallel for
 		for (int i = 0; i < nq; i++) {
-			ft[i] = 0;
+			ft[i] = COMPLX_ZERO;
 			for (int j = 0; j < npts; j++) {
 				float q_r = 0.f;
 				for (int k = 0; k < 3; k++) q_r += qvs[3*i + k] * pts[3*j + k];
@@ -41,7 +43,7 @@ py::array py_dft(py::array_t<float, py::array::c_style | py::array::forcecast> P
 		}
 		return result;
 }
-		
+
 PYBIND11_MODULE (mdscatter, m) {
 		m.def("dft", &py_dft, "Compute Discrete Fourier Transform at precribed q-points");
 }
